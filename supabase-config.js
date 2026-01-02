2c972959bd330ef1e426d6214daae42cf070a4b9
tkshmrkm <mrkn55@gmail.com>
2026-01-02 21:27:28 +0900

    Add files via upload

--- supabase-config.js
+++ supabase-config.js
@@ -3,6 +3,7 @@ const SUPABASE_URL = 'https://qtnqcupqfgmheeapbzja.supabase.co';
 const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InF0bnFjdXBxZmdtaGVlYXBiemphIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjczNTA1NTAsImV4cCI6MjA4MjkyNjU1MH0.-7PY7aT1gKVbrLJW38YhWH9Eun8Wh7e32oIy_O2c4P4';
 
 // Supabaseクライアント初期化（CDN版）
+// CDNで読み込まれたsupabaseライブラリからcreateClientを取得
 const supabaseClient = supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
 
 // app.jsで使うためにグローバル変数を上書き
